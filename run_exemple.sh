1c56481df4eb0d32fd3e10166065b38865af2aa9
amelietn <92325683+amelietn@users.noreply.github.com>
2024-06-19 14:05:03 +0200

    Update run_exemple.sh

--- run_exemple.sh
+++ run_exemple.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # launch nextflow pipeline with two parameters
-nextflow run main.nf --input ../data/dataset_test_TGBEK12/ --out_dir results_new --ref data/rRNA.fa
+nextflow run main.nf --input .../fastqs/ --barcodes .../barcodes/
