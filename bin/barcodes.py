137fa272f162e9b63fc4dc4c31e3d0b4437c6d91
amelietn <92325683+amelietn@users.noreply.github.com>
2024-06-21 12:59:51 +0200

    Update barcodes.py

--- bin/barcodes.py
+++ bin/barcodes.py
@@ -10,12 +10,12 @@ from Bio import SeqIO
 import argparse
 import csv
 
-def main(fastq_file, barcodes_dir, output_dir):
+def main(fastq_file, barcodes_dir, output_dir, save_plot=False):
     subprocess.call(f"zcat {fastq_file} | head -100000 > FASTQ_sub.fq", shell=True)    # to read first 100k lines of FASTQ file
 
-    barcodes_analysis(barcodes_dir, output_dir)
+    barcodes_analysis(barcodes_dir, output_dir, save_plot)
 
-def barcodes_analysis(barcodes_dir, output_dir):    
+def barcodes_analysis(barcodes_dir, output_dir, save_plot=False):    
     # Alithea's barcodes
     barcodes_files = glob.glob(os.path.join(barcodes_dir, '*brb.txt'))
     barcodes_names = [os.path.splitext(os.path.basename(bfiles))[0] for bfiles in barcodes_files]
@@ -80,22 +80,23 @@ def barcodes_analysis(barcodes_dir, output_dir):
     # Barplot
     barcodes = [item[0] for item in percentages]
     values = [item[1] for item in percentages]
-    barplot = plt.figure(figsize=(10, 6))
-    plt.bar(barcodes, values)
-    plt.xlabel('Barcodes')
-    plt.xticks(fontsize=6, rotation=45, ha='right')
-    plt.ylabel('Percentage of Reads')
-    plt.title(f'Percentage of reads associated with each barcode for a given library')
-    plt.ylim(0, 100)
-    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.4)
-    ax = plt.gca()
-    xticks = ax.get_xticklabels()
-    for label in xticks:           
-        if label.get_text() == chosen_barcode:
-            label.set_color('red')
-    output_file = os.path.join(output_dir, 'Barcode_analysis.pdf')
-    plt.savefig(output_file, format='pdf', bbox_inches='tight')    
-    #plt.show()
+    if save_plot:
+        barplot = plt.figure(figsize=(10, 6))
+        plt.bar(barcodes, values)
+        plt.xlabel('Barcodes')
+        plt.xticks(fontsize=6, rotation=45, ha='right')
+        plt.ylabel('Percentage of Reads')
+        plt.title(f'Percentage of reads associated with each barcode for a given library')
+        plt.ylim(0, 100)
+        plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.4)
+        ax = plt.gca()
+        xticks = ax.get_xticklabels()
+        for label in xticks:           
+            if label.get_text() == chosen_barcode:
+                label.set_color('red')
+        output_file = os.path.join(output_dir, 'Barcode_analysis.pdf')
+        plt.savefig(output_file, format='pdf', bbox_inches='tight')    
+        plt.close()
 
     # Saving to .csv
     table_data = [(f'{item[0]}', f'{item[1]}') for item in percentages]  
@@ -110,6 +111,7 @@ if __name__ == "__main__":
     parser.add_argument("fastq_file", help="Path to the FASTQ file")
     parser.add_argument("barcodes_dir", help="Path to the directory containing barcode files")
     parser.add_argument("output_dir", help="Directory to save the output plot")
+    parser.add_argument("--plot", action='store_true', help="Option to save plot as Barcode_analysis.pdf")
     args = parser.parse_args()
 
-    main(args.fastq_file, args.barcodes_dir, args.output_dir)
+    main(args.fastq_file, args.barcodes_dir, args.output_dir, args.plot)
